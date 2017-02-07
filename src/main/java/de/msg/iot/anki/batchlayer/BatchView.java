b6a00fb4890f56a73295e0c7b6babb2d8d69d5e1
Adrian Weber <adrian@kuebler-weber.de>
2017-02-07 12:54:23 +0100

    Initial Commit with LA components.

---
+++ src/main/java/de/msg/iot/anki/batchlayer/BatchView.java
@@ -0,0 +1,7 @@
+package de.msg.iot.anki.batchlayer;
+
+/**
+ * Created by aweber on 07.02.17.
+ */
+public interface BatchView {
+}
---
+++ src/main/java/de/msg/iot/anki/speedlayer/SpeedView.java
@@ -0,0 +1,7 @@
+package de.msg.iot.anki.speedlayer;
+
+/**
+ * Created by aweber on 07.02.17.
+ */
+public interface SpeedView {
+}
