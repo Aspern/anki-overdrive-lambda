cac266fc200e1a87d35244452902c66605f1c226
Adrian Weber <adrian@kuebler-weber.de>
2017-02-07 15:49:20 +0100

    added settings module. added dependency injection support.

---
+++ src/main/java/de/msg/iot/anki/connector/ConnectorModule.java
@@ -0,0 +1,12 @@
+package de.msg.iot.anki.connector;
+
+import com.google.inject.AbstractModule;
+
+
+public class ConnectorModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
