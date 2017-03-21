2e048c9dee11355d98a61a73f6b01868e18112db
Muhammad Salman Sayani <sayani@Muhammads-MacBook-Pro.local>
2017-03-21 17:12:29 +0100

    adding rest script

--- web/js/angular_main.js
+++ web/js/angular_main.js
@@ -1,11 +1,51 @@
 'use strict';
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['PostService']);
+var response;
+
+app.controller('MyCtrl', function($scope , $timeout, SendPostReq){
+
+  
+
+  $scope.sendReq = function (url,data) {
+    
+    response = SendPostReq.sendPost(url,data);
+
+    response.success(function (response) {
+        console.log('Url sent');
+      });
+
+    response.error(function (response) {
+        console.log('Error');
+      });
+
+    if(data == 'AC-S')
+    {
+    		console.log("starting Anti collision");
+    }
+
+    else if(data == 'AC-I')
+    {
+    		console.log("Stoping Anti collision");
+
+    }
+
+    else if(data = "C-S")
+    {
+    		console.log("starting collision");
+    	
+    }
+
+    else if(data = "C-I")
+    {
+    		console.log("Stoping collision");
+    	
+    }
+
+	
+	};
 
 
-app.controller('MyCtrl', function($scope , $timeout){
-
-  console.log("hello hi ");
 	
 	
 	
